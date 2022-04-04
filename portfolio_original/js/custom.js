096922b9fed492f4e9dd163484cd3bd2ab27714a
gh1343 <89381440+gh1343@users.noreply.github.com>
2022-04-04 23:21:50 +0900

    Add files via upload

---
+++ portfolio_original/js/custom.js
@@ -0,0 +1,20 @@
+
+$(function () {
+	// $("html,body").stop().animate({"scrollTop":0},1000);
+
+	$(document).ready(function () {
+		$('#fullpage').fullpage({
+			//options here
+			licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+			autoScrolling: true,
+			scrollHorizontally: true,
+			responsive: 768,
+			responsiveWidth: 768,
+			navigation: true,
+			navigationPosition: 'right',
+			sectionSelector: '.section'
+		});
+	});
+
+});
+
