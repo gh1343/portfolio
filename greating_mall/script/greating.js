884aff4be53586577070c9aff24da1a6b6d0ea85
DESKTOP-VR7PR78\user <boysheep@naver.com>
2024-05-06 22:44:08 +0900

    그리팅몰 수정

--- greating_mall/script/greating.js
+++ greating_mall/script/greating.js
@@ -60,4 +60,103 @@ $(document).ready(function(){
     $('.mid_banner_slide_wrap').on('mouseleave', function(){
         mid_banner_slide_wrap.autoplay.start();
     });
+
+    // part3 탭
+    $('.early_choice li').click(function() {
+        const click_btn_Id = $(this).attr('id');
+        const content_Class = '.' + click_btn_Id;
+        const content_Wrap = $('.part3 .article_wrap > div');
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        content_Wrap.css('display', 'none').filter(content_Class).css('display', 'flex');
+    });
+    // part5 슬라이드
+    const mbr1 = document.querySelector(".part5_arrow.right");
+    const mbl1 = document.querySelector(".part5_arrow.left");
+    const part5_slide_wrap = new Swiper(".part5_slide_wrap", {
+        slidesPerView : 4,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: ".part5_arrow.right",
+            prevEl: ".part5_arrow.left",
+        },
+        on: {
+            init: function () {
+                // 초기화될 때 왼쪽 버튼 숨기기
+                mbl1.style.display = 'none';
+            },
+            slideChange: function () {
+                // 현재 슬라이드 인덱스 가져오기
+                let currentIndex1 = this.realIndex;
+                if (window.innerWidth > 1024) {
+                    // 첫 번째 슬라이드에 도달했을 때
+                    if (this.isBeginning) {
+                        // 왼쪽 버튼 숨기기
+                        mbl1.style.display = 'none';
+                    } else {
+                        // 첫 번째 슬라이드가 아닌 경우 왼쪽 버튼 보이기
+                        mbl1.style.display = 'block';
+                    }
+                    // 마지막 슬라이드에 도달했을 때
+                    if (this.isEnd) {
+                        // 오른쪽 버튼 숨기기
+                        mbr1.style.display = 'none';
+                    } else {
+                        // 마지막 슬라이드가 아닌 경우 오른쪽 버튼 보이기
+                        mbr1.style.display = 'block';
+                    }
+                } else {
+                    if (this.isBeginning) {
+                        mbl1.style.display = 'none';
+                        mbr1.style.display = 'none';
+                    } else {
+                        mbl1.style.display = 'none';
+                        mbr1.style.display = 'none';
+                    }
+                    if (this.isEnd) {
+                        mbr1.style.display = 'none';
+                        mbl1.style.display = 'none';
+                    } else {
+                        mbr1.style.display = 'none';
+                        mbl1.style.display = 'none';
+                    }
+                }
+            },
+        },
+    });
+    // part6 리뷰슬라이드
+    const review_slide_wrap = new Swiper(".review_slide_wrap", {
+        slidesPerView : 4.2,
+        spaceBetween: 12,
+    });
+    // part7 매거진 슬라이드
+    const part7_slide_wrap = new Swiper(".part7_slide_wrap", {
+        loop: true,
+        slidesPerView : 3,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".part7_arrow.right",
+            prevEl: ".part7_arrow.left",
+        },
+    });
+    $('.tab_list_wrap > li').click(function (){
+        const click_list_Id = $(this).attr('id');
+        const slide_Class = '.' + click_list_Id;
+        const slide_Wrap = $('.slide_wrap > div');
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        slide_Wrap.css('display', 'none').filter(slide_Class).css('display', 'block');
+    });
+    // 공지사항 슬라이드
+     // part7 매거진 슬라이드
+    const notice_wrap = new Swiper(".notice_wrap", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        slidesPerView : 1,
+        spaceBetween: 10,
+        direction: "vertical",
+    });
 });
